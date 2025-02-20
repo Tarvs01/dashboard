288f275af88400f63e562a61a9c45ebe96462759
Terver Torvenda <tervenda18@gmail.com>
2025-02-20 09:09:18 +0100

    Made some aesthetic changes to the small sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -291,11 +291,24 @@ function Sidebar({setPage, isSidebarOpen, setIsSidebarOpen} : SidebarProps) {
 
       <div className="sidebar-small-cont" style={{display: `${isSidebarOpen ? "block" : "none"}`}} onClick={closeSmallSidebar}>
       <div className="sidebar-small">
-        <div className="logo">
-          <h2>SPECTRA</h2>
-          <div onClick={closeSmallSidebar}>
+        <div className="sidebar-small-header">
+        <div onClick={closeSmallSidebar}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
           </div>
+
+          <div className="log-out">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
+            </svg>
+          </div>
+          <span>Log out</span>
+        </div>
+
+        </div>
+
+        <div className="logo">
+          <h2>SPECTRA</h2>
         </div>
 
         <ul className="links-container">
@@ -419,15 +432,6 @@ function Sidebar({setPage, isSidebarOpen, setIsSidebarOpen} : SidebarProps) {
             </div>
           </li>
         </ul>
-
-        <div className="log-out">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
-            </svg>
-          </div>
-          <span>Log Out</span>
-        </div>
       </div>
       </div>
     </aside>
