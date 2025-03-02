fba52bc8289ac1a1125ed19d039ff3cdccbb9354
Terver Torvenda <tervenda18@gmail.com>
2025-03-02 11:29:16 +0100

    Made responsive changes to Contacts page

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1185,6 +1185,10 @@ function HandleSelectChange(e: ChangeEvent<HTMLSelectElement>){
               <h4 className={`cn-job ${isSplitPageShown ? "disappear" : ""}`}>{singleContact.job}</h4>
               <h4 className={`cn-mail ${isSplitPageShown ? "disappear" : ""}`}>{singleContact.email}</h4>
               <h4 className={`cn-phone ${isSplitPageShown ? "disappear" : ""}`}>{singleContact.phone}</h4>
+
+              <div className={`cn-more ${isSplitPageShown ? "disappear" : ""}`}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512"><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z"/></svg>
+              </div>
             </div>
           })
         }
