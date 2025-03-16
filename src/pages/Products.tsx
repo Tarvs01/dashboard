ce074e4f8c3cd5958c8b1644dcd70d33442823f9
Terver Torvenda <tervenda18@gmail.com>
2025-03-16 14:09:27 +0100

    Added edit functionality to the products. Also fixed some logic bugs

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -151,7 +151,7 @@ function Products() {
 
   function handleCategorySelectChange(e: ChangeEvent<HTMLSelectElement>){
     if(e.target.value === "All"){
-      setAllProducts(baseProducts);
+      setDisplayedProducts(allProducts);
       setFilter("All");
     }
     else{
@@ -170,6 +170,9 @@ function Products() {
       if(e.currentTarget.value.trim() !== ""){
         setSingleProduct({...singleProduct, name: e.currentTarget.value});
       }
+      else{
+        setSingleProduct({...singleProduct, name: ""});
+      }
     }
     else if(prop === "price"){
       let tempPrice = Number(e.currentTarget.value);
@@ -182,6 +185,9 @@ function Products() {
     else if(prop === "tag"){
       if(e.currentTarget.value.trim() !== ""){
         setCurrentTag(e.currentTarget.value);
+      }
+      else{
+        setCurrentTag("");
       }
     }
     else{
@@ -202,7 +208,7 @@ function Products() {
     setSingleProduct({...singleProduct, tags: newTags});
   }
 
-  function setProductChange(state = undefined){
+  function setProductChange(state: number | undefined = undefined){
     if(state == undefined){
       let currentState: ProductType = {
         name: "",
@@ -217,6 +223,7 @@ function Products() {
     }
     else{
       let currentState = allProducts.filter((product) => product.id === state)[0];
+      currentState = JSON.parse(JSON.stringify(currentState));
       setSingleProduct(currentState);
     }
   }
@@ -237,12 +244,31 @@ function Products() {
       let tempDisplayedProducts = tempAllProducts.filter((prod) => prod.category === filter);
       setDisplayedProducts(tempDisplayedProducts);
     }
-    
+
     setOpenProductModal("");
   }
 
   function editProduct(){
-    ;
+    const tempProduct: ProductType = JSON.parse(JSON.stringify(singleProduct));
+    let tempAllProducts = allProducts.map((product) => {
+      if(product.id === tempProduct.id){
+        return tempProduct;
+      }
+      else{
+        return product;
+      }
+    });
+    setAllProducts(tempAllProducts);
+
+    if(filter === "All"){
+      setDisplayedProducts(tempAllProducts);
+    }
+    else{
+      let tempDisplayedProducts = tempAllProducts.filter((prod) => prod.category === filter);
+      setDisplayedProducts(tempDisplayedProducts);
+    }
+
+    setOpenProductModal("");
   }
 
   return (
@@ -323,7 +349,7 @@ function Products() {
                   <h4>{product.name}</h4>
                   <p>${product.price}</p>
                   <div className="product-action-cont">
-                    <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg><span>Edit</span></button>
+                    <button onClick={() => {setProductChange(product.id) ;setOpenProductModal("edit")}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg><span>Edit</span></button>
                     <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg><span>Delete</span></button>
                   </div>
                 </div>
